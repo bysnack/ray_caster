86f201db363eebbb795f34308aceb884a1df730a
bysnack <andres@summitto.com>
2019-10-30 21:18:37 +0100

    comment user class

--- User.h
+++ User.h
@@ -1,58 +1,94 @@
 #pragma once
 
-#include <iostream>
-#include <SFML/Graphics.hpp>
-#include <cmath>
-#include <cstdint>
-#include <vector>
-#include <array>
 #include "Map.h"
 #include "Ray.h"
 #include "Utilities.h"
+#include <SFML/Graphics.hpp>
+#include <array>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <vector>
 
-
+/**
+ * @brief  User class
+ */
 class User : public sf::Drawable {
 public:
-  User(const Map& map) : 
-    _current_map{ map }
-  {
+  /**
+   * @brief         Constructor
+   */
+  User(const Map &map) noexcept : _current_map{map} {
+    // set user properties
     _user.setFillColor(sf::Color::Blue);
     _user.setPosition(_position);
     _user.setOrigin(_user.getRadius(), _user.getRadius());
   }
 
+  /**
+   * @brief         Captures keyboard input used to move the player around
+   */
   void captureMovement() noexcept {
-    const sf::Vector2f xUnity{ 1.f * _speed, 0.f };
-    const sf::Vector2f yUnity{ 0.f, 1.f * _speed };
+    // unity advance vectors
+    const sf::Vector2f xUnity{1.f * _speed, 0.f};
+    const sf::Vector2f yUnity{0.f, 1.f * _speed};
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))  setPosition(_position + xUnity);
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))   setPosition(_position - xUnity);
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))   setPosition(_position + yUnity);
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))     setPosition(_position - yUnity);
+    // on key pressed
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+      setPosition(_position + xUnity);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+      setPosition(_position - xUnity);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+      setPosition(_position + yUnity);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+      setPosition(_position - yUnity);
   }
 
-  void setPosition(const sf::Vector2f& pos) noexcept {
+  /**
+   * @brief           Sets the current user position to a given position
+   * @param position  Screen coordinates where the user is going to be positioned
+   */
+  void setPosition(const sf::Vector2f &position) noexcept {
+    // check for collisions
     for (uint32_t i = 0; i < 4; ++i) {
-      switch(i) {
-        case 0:
-          if (_current_map.isColiding(pos + sf::Vector2f(0.f, _user.getRadius()))) return;
-          break;
-        case 1:
-          if (_current_map.isColiding(pos - sf::Vector2f(0.f, _user.getRadius()))) return;
-          break;
-        case 2:
-          if (_current_map.isColiding(pos + sf::Vector2f(_user.getRadius(), 0.f))) return;
-          break;
-        default:
-          if (_current_map.isColiding(pos - sf::Vector2f(_user.getRadius(), 0.f))) return;
-          break;
+      switch (i) {
+      case 0:
+        if (_current_map.isColiding(position +
+                                    sf::Vector2f(0.f, _user.getRadius())))
+          return;
+        break;
+      case 1:
+        if (_current_map.isColiding(position -
+                                    sf::Vector2f(0.f, _user.getRadius())))
+          return;
+        break;
+      case 2:
+        if (_current_map.isColiding(position +
+                                    sf::Vector2f(_user.getRadius(), 0.f)))
+          return;
+        break;
+      default:
+        if (_current_map.isColiding(position -
+                                    sf::Vector2f(_user.getRadius(), 0.f)))
+          return;
+        break;
       }
     }
-    _position = pos;
+
+    // update user position
+    _position = position;
+
+    // update circle position in screen
     _user.setPosition(_position);
+
+    // cast rays on player position change to avoid unused of calculations
     castRays(1000);
   }
 
+  /**
+   * @brief           Cast a given amount of rays
+   * @param amount    The amount of rays to cast
+   */
   void castRays(uint32_t amount) {
     // clear old set of rays
     _rays.clear();
@@ -67,16 +103,20 @@ public:
   }
 
 private:
-  virtual void draw(sf::RenderTarget& target, sf::RenderStates state) const override {
-    for (auto&& ray : _rays) {
+  /**
+   * @brief           Draws the user and rays to the screen
+   * @param target    Target where the elements are going to be drawn
+   */
+  virtual void draw(sf::RenderTarget &target, sf::RenderStates) const override {
+    for (auto &&ray : _rays) {
       target.draw(ray);
     }
     target.draw(_user);
   }
-  
-  sf::CircleShape   _user         { 10.f          };
-  sf::Vector2f      _position     { 400.f, 300.f  };
-  float             _speed        { 5.f           };
-  std::vector<Ray>  _rays         {               };
-  const Map         &_current_map                  ;
+
+  sf::CircleShape _user{10.f};
+  sf::Vector2f _position{400.f, 300.f};
+  float _speed{5.f};
+  std::vector<Ray> _rays{};
+  const Map &_current_map{};
 };
